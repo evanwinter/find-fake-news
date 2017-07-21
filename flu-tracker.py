6924a29b889e5cde2a630b3c4cb42eae9f1fef81
Evan Winter <ewinter1996@gmail.com>
2017-07-21 13:18:33 -0500

    .

--- flu-tracker.py
+++ flu-tracker.py
@@ -17,58 +17,25 @@ if (not api):
 	print ("Can't authenticate")
 	sys.exit(-1)
 
-def favorite_ten_tweets():
-	for tweet in tweepy.Cursor(api.home_timeline).items(10):
-		# print the tweet
-	    print("\n"+tweet.text)
-
-	    # store the tweet data
-	    process_or_store(tweet._json)
-
-	    # add tweet to favorites
-	    add_to_favorites(tweet)
-
-def process_or_store(tweet):
-	with open('data.txt', 'a') as outfile:
-		json.dump(tweet, outfile, sort_keys = True, indent = 4,
-		               ensure_ascii = False)
-
-def add_to_favorites(tweet):
-	if not tweet.favorited:
-		try:
-			api.create_favorite(tweet.id)
-			print("Added to favorites.")
-		except tweepy.TweepError:
-			sys.exit("Tweep Error")
-		except Exception as e:
-			print(e)
-			pass
-	else:
-		print("You already favorited this tweet.")
-		pass
-
-
 # start getting tweets
-print("Getting flu-related tweets...\n")
-
-# favorite and store ten most recent tweets from timeline
-# favorite_ten_tweets()
+print("\nGetting flu-related tweets...\n")
 
 class MyListener(StreamListener):
  
-    def on_data(self, data):
+    def on_data(self, tweet):
         try:
             with open('tweets.json', 'a') as f:
-                f.write(simplejson.dumps(simplejson.loads(data), indent=4, sort_keys=True))
+                f.write(simplejson.dumps(simplejson.loads(tweet), indent=4, sort_keys=True))
                 print("------------------\n-- tweet stored --\n------------------\n")
+                print(tweet.location)
                 return True
         except BaseException as e:
             print("Error on_data: %s" % str(e))
-        return True
+        return True # don't kill stream
  
     def on_error(self, tweet):
         print(tweet)
-        return True
+        return True	# don't kill stream
  
 twitter_stream = Stream(auth, MyListener())
 twitter_stream.filter(track=['the flu'])
