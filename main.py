d43d569222d3823790dddcdb6f4f8a4e0fa6f571
Evan Winter <ewinter1996@gmail.com>
2017-07-25 14:04:58 -0500

    .

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import sys
 
 auth = OAuthHandler(config.consumer_key, config.consumer_secret)
 auth.set_access_token(config.access_token, config.access_secret)
- 
+
 api = tweepy.API(auth)
 
 # make sure we authenticate
