4280f8f7b54d61e3e3108b3387092c28cea9358c
Evan Winter <ewinter1996@gmail.com>
2017-07-25 15:28:48 -0500

    .

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if (not api):
 	sys.exit(-1)
 
 # start getting tweets
-print("\nGetting flu-related tweets...\n")
+print("\nGetting tweets...\n")
 
 class MyListener(StreamListener):
 
@@ -25,14 +25,19 @@ class MyListener(StreamListener):
         try:
             with open('tweets.json', 'a') as f:
                 tweetdict = json.loads(tweet)
-                if (tweetdict['coordinates']):
+                # only store data if it fits our criteria
+                if ("fake news" in tweetdict['text'] and "RT" not in tweetdict['text']):
+                    # write to json and make it pretty
                     f.write(json.dumps(tweetdict, indent=4, sort_keys=True))
+
+                    # log it
                     print("Tweet stored.")
-                    print(tweetdict['coordinates'])
+                    print(tweetdict['text'])
                     print()
+
                 else:
-                    print("Tweet not stored -- no location data.")
-                    print()
+                    # print("Tweet not stored")
+                    pass
                 return True
         except BaseException as e:
             print("Error on_status: %s\n" % str(e))
@@ -42,4 +47,4 @@ class MyListener(StreamListener):
         return True	# don't kill stream
  
 twitter_stream = Stream(auth, MyListener())
-twitter_stream.filter(track=['flu'])
+twitter_stream.filter(track=['fake news'])
