aeaee3e58a835d94aaef6842d3068b21298447b7
Evan Winter <ewinter1996@gmail.com>
2017-07-25 13:38:06 -0500

    .

--- main.py
+++ main.py
@@ -21,25 +21,25 @@ print("\nGetting flu-related tweets...\n")
 
 class MyListener(StreamListener):
  
-    def on_status(self, tweet):
-        try:
-            with open('tweets.txt', 'a') as f:
-                f.write(tweet.text+'\n')
-                print("Tweet Stored:\n"+tweet.text+"\n")
-                return True
-        except BaseException as e:
-            print("Error on_status: %s\n" % str(e))
-        return True # don't kill stream
-
-    # def on_data(self, tweet):
+    # def on_status(self, tweet):
     #     try:
-    #         with open('tweets.json', 'a') as f:
-    #         	f.write(json.dumps(json.loads(tweet), indent=4, sort_keys=True))
-    #         	print("Tweet Stored:\n"+tweet.text+"\n")
-    #         	return True
+    #         with open('tweets.txt', 'a') as f:
+    #             f.write(tweet.text+'\n')
+    #             print("Tweet Stored:\n"+tweet.text+"\n")
+    #             return True
     #     except BaseException as e:
     #         print("Error on_status: %s\n" % str(e))
     #     return True # don't kill stream
+
+    def on_data(self, tweet):
+        try:
+            with open('tweets.json', 'a') as f:
+            	f.write(json.dumps(json.loads(tweet), indent=4, sort_keys=True))
+            	# print("Tweet Stored:\n"+tweet.text+"\n")
+            	return True
+        except BaseException as e:
+            print("Error on_status: %s\n" % str(e))
+        return True # don't kill stream
  
     def on_error(self, tweet):
         print(tweet)
--- readme.md
+++ readme.md
@@ -2,5 +2,6 @@
 
 * http://www.politifact.com/punditfact/article/2017/apr/20/politifacts-guide-fake-news-websites-and-what-they/
 * https://medium.com/@JBramVB/mapping-happiness-with-twitter-natural-language-processing-ac231e70fe7
+* https://github.com/jbrambleDC/Twitter-NLPMaps
 
 ### to do
