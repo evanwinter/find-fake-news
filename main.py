9c202133f12cdbbfb68f595be61eb36c34f764c7
Evan Winter <ewinter1996@gmail.com>
2017-08-01 15:11:49 -0500

    if quote rt, get quoted url instead of url to quoted tweet'

--- main.py
+++ main.py
@@ -28,35 +28,31 @@ class MyListener(StreamListener):
         try:
             with open('tweets.json', 'a') as f:
                 
-                # make it a dictionary
                 tweetdict = json.loads(tweet)
 
-                # find the list of urls in the tweet data
-                tweet_urls = tweetdict['entities']['urls']
+                is_quote_status = tweetdict['is_quote_status']
+                tweet_urls = ''
+
+                if (is_quote_status == False):
+                    # look for this status urls
+                    tweet_urls = tweetdict['entities']['urls']
+                else:
+                    # look for quote status urls
+                    tweet_urls = tweetdict['quoted_status']['entities']['urls']
 
                 if (len(tweet_urls) > 0):
-                    
                     full_url = tweet_urls[0]['expanded_url']
 
                     if (full_url is not None):
-
-                        # write the full tweet data
                         f.write(json.dumps(tweetdict, indent=4, sort_keys=True))
-
-                        # or just the url
-                        # f.write(json.dumps(full_url, indent=4, sort_keys=True)+'\n')
+                        f.write(json.dumps('\n'+full_url)+'\n')
 
                         print('\nTweet stored.')
                         print(full_url)
 
                     else:
-                        # print('Has no expanded URL')
-                        # print('Tweet DISCARDED')
                         pass
-
                 else:
-                    # print('Has no URLs')
-                    # print('Tweet DISCARDED')
                     pass
 
                 return True
